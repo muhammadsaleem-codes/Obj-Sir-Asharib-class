8862f26061f4a9c2310dbe383098a6ce386676b5
muhammad-s381 <muhammadsaleembaloch798@gmail.com>
2026-09-21 22:28:21 +0500

    Add JavaScript object practice

---
+++ app.js
@@ -0,0 +1,137 @@
+// var students = [
+//     {
+//         name: "Muhammad",
+//         age: 24,
+//         class: "12th",
+//         marks: [90, 78, 44, 56, 90],
+//         //id:123
+//     },
+//     {
+//         name: "Bilal",
+//         age: 20,
+//         class: "11th",
+//         marks: [60, 70, 70, 80, 80],
+
+//     }
+
+// ];
+
+// console.log(students);
+// var users = [   //array of objects
+//     {
+//     username: 'person 2',
+//     email: 'person2@abc.com',
+//     age: 22,
+//     contacts: ['03111111', '03222222'],
+    
+// },
+//     {
+//     username: 'person 2',
+//     email: 'person2@abc.com',
+//     age: 22,
+//     contacts: ['03111111', '03222222'],
+// },
+//    {
+//     username: 'person 2',
+//     email: 'person2@abc.com',
+//     age: 23,
+//     contacts: ['03111111', '03222222'],
+// },
+// ]
+
+// var checkProperty = "id" in students[0];
+
+// console.log(checkProperty);
+// // console.log(users[0].age);
+// // console.log(users[1]);
+
+
+// var st1={
+//     name:'ali',
+//     age: 23,
+//     section: 'A',
+//     marks:[50, 60, 89, 90, 80,],
+//     calPer:function() {
+//         var obtMarks=0;
+//         for (var i=0; i<this.marks.length; i++){
+//             obtMarks+=this.marks[i]
+             
+            
+//         }
+//         return ((obtMarks/500)*100)
+        
+        
+//     }
+   
+// };
+
+// var studendPer=st1.calPer()
+// console.log(studendPer)
+// console.log(st1)
+
+
+
+// var student = {
+//     name: "Ali",
+//     marks: [50, 60, 70, 80, 90],
+
+//     calTotal: function() {
+//         var obtmarks=0;
+//         for(var i=0; i<this.marks.length; i++){
+//             obtmarks+=this.marks[i]
+
+//         }
+
+//       console.log(obtmarks)
+//     }
+// };
+//  var students=student.calTotal()
+//  console.log(student)
+
+
+
+
+// var student = {
+//     name: "Ali",
+//     marks: [50, 60, 70, 80, 90],
+
+//     calAverage: function() {
+//         var total=0;
+//    for(var i =0; i<this.marks.length; i++){
+//        total=total+this.marks[i]
+
+//    }
+//     return (total/this.marks.length)
+//     },
+  
+
+
+
+
+// }
+
+
+// var students=student.calAverage()
+//  console.log(students)
+
+
+//  var student = {
+//     name: "Ali",
+//     marks: [50, 60, 70, 80, 90],
+
+//     highestMarks: function() {
+//         var highest = this.marks[0];
+
+//         for (var i = 1; i < this.marks.length; i++) {
+//             if (this.marks[i]  highest) {
+//                 highest = this.marks[i];
+//             }
+//         }
+
+//         return highest;
+//     }
+// };
+
+// console.log(student.highestMarks());
+
+
